a5cb779418647a7fc3b64b78ad45adc7a7a66a1a
mohammadokla1999 <140254042+mohammadokla1999@users.noreply.github.com>
2023-07-22 15:59:15 +0300

    Add files via upload

---
+++ muhammet_okla_Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ur_fiyati,kargo_ucreti, toplama;
+            Console.Write("ürün fiyatı : "); 
+            ur_fiyati = double.Parse(Console.ReadLine());
+
+            if (ur_fiyati >= 100)
+            {
+                Console.WriteLine("kargo ücretsiz");
+            } 
+            else if(ur_fiyati < 100) 
+            {
+                kargo_ucreti = 5;
+                toplama = ur_fiyati + kargo_ucreti;
+                Console.WriteLine("kargoya  5'Tl fazla alınacak " + ":" + toplama);
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
